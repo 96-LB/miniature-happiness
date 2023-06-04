475748d59bdad2c864b790f04f8211153fc25330
Sanketh Varamballi <sankethv03@gmail.com>
2023-06-04 12:22:35 -0700

    optional weights and biases

--- ai/neuralnet.py
+++ ai/neuralnet.py
@@ -6,14 +6,23 @@ def sigmoid(Z):
 	A = 1/(1+np.exp(np.dot(-1, Z)))
 
 class NeuralNet:
-    def __init__(self, layer_dims: list[int]):
+    def __init__(self, layer_dims, weights = None, biases = None):
         self.L = len(layer_dims)
         self.layer_dims = layer_dims[:]
-        self.weights = [None] # first layer has no weights
-        self.biases = [None] # first layer has no biases
-        for layer in range(1, self.L):
-            self.weights.append(np.random.randn(layer_dims[layer - 1], layer_dims(layer))) 
-            self.biases.append(np.zeros((layer_dims[layer], 1))) 
+
+        if weights is None:
+            self.weights = [None] # first layer has no weights
+            for layer in range(1, self.L):
+                self.weights.append(np.random.randn(layer_dims[layer - 1], layer_dims(layer))) 
+        else:
+            self.weights = weights[:]
+
+        if biases is None:
+            self.biases = [None] # first layer has no biases
+            for layer in range(1, self.L):
+                self.biases.append(np.zeros((layer_dims[layer], 1))) 
+        else:
+            self.biases = biases[:]
 
     def forward_prop(self, X):
         A = X
